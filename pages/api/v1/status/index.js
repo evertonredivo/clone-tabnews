9638793ef1f2c9bff28d6c72408ff10d594106e7
Everton Redivo <evertonredivo@gmail.com>
2025-04-26 09:41:39 -0300

    Add `database.js` and `.env` files.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,13 @@
-function status(request, response) {
-  response.status(200).json({ "API Endpoint": "Status" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const endpoint_name = "Status";
+  const result = await database.query("SELECT current_database();");
+  const current_database = result.rows[0].current_database;
+  response.status(200).json({
+    "API Endpoint": endpoint_name,
+    "Current Database": current_database,
+  });
 }
 
 export default status;
