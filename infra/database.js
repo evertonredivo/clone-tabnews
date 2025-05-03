c7e04fe140818f57e2fcc66510e77685a969b1d3
Everton Redivo <evertonredivo@gmail.com>
2025-05-03 16:38:37 -0300

    Adds `/api/v1/migrations` endpoint.

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,9 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
-  });
-
+  let client;
   try {
-    await client.connect();
+    client = await getNewConnectedClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
@@ -22,6 +14,20 @@ async function query(queryObject) {
   }
 }
 
+async function getNewConnectedClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "production" ? true : false,
+  });
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewConnectedClient,
 };
