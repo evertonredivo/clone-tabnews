340365efcd5878ecca9a7bf11b8b2f52bb73ff29
Everton Redivo <evertonredivo@gmail.com>
2025-05-11 14:10:22 -0300

    Makes `npm test` motr robust with `async-retry` and orchestrator.js`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,9 @@
+import orquestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orquestrator.waitForAllServices();
+});
+
 test("GET to /api/v1/status should return HTTP status code = 200;", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
