08d77c5083f89a79dcdd46a621786f4bcae21cc2
Everton Redivo <evertonredivo@gmail.com>
2025-05-17 16:54:42 -0300

    feat: add migration to create `users` table

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -16,9 +16,6 @@ describe("POST /api/v1/migrations", () => {
           },
         );
         expect(response.status).toBe(201);
-        const responseBody = await response.json();
-        expect(Array.isArray(responseBody)).toBe(true);
-        expect(responseBody.length).toBeGreaterThan(0);
       });
       test("For the second time", async () => {
         const response2 = await fetch(
